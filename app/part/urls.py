4e41829afb0fed7fdb15da438500534f47ee2d4a
Ekaterina Borovikova <borovikova@zoho.eu>
2021-01-04 16:10:01 +0300

    Start aggregated data endpoint

--- app/part/urls.py
+++ app/part/urls.py
@@ -1,11 +1,19 @@
 from django.urls import path
+from rest_framework.routers import DefaultRouter
 
 from part import views
 
 
 app_name = 'part'
 
-urlpatterns = [
-    path('data/', views.PartViewSet.as_view({'get': 'list'}), name='data'),
-    path('filters/', views.FiltersViewSet.as_view(), name='filters'),
-]
+router = DefaultRouter()
+router.register(r'data', views.PartViewSet, basename='data')
+router.register(r'filters', views.FiltersViewSet, basename='filters')
+router.register(r'aggregated_data', views.AggregatedDataViewSet, basename='aggregated_data')
+urlpatterns = router.urls
+
+# urlpatterns = [
+#     path('data/', views.PartViewSet.as_view({'get': 'list'}), name='data'),
+#     path('filters/', views.FiltersViewSet.as_view(), name='filters'),
+#     path('aggregated_data/', views.AggregatedDataViewSet.as_view({'get': 'list'}), name='aggregated_data'),
+# ]
--- app/part/views.py
+++ app/part/views.py
@@ -1,5 +1,6 @@
 import re
 import pandas as pd
+import datetime
 
 from rest_framework import generics, authentication, permissions
 from rest_framework.authentication import TokenAuthentication
@@ -10,7 +11,6 @@ from rest_framework import viewsets, mixins, response, views
 from .models import Part
 from part import serializers
 
-# @swagger_auto_schema(method='get', auto_schema=None)
 class PartViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
     """Не принимает никаких параметров, возвращает все имеющиеся в базе данные одним ответом.
     В Headers запроса должен быть токен `Authorization: Token <token>`"""
@@ -20,7 +20,7 @@ class PartViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
     serializer_class = serializers.PartSerializer
 
 
-class FiltersViewSet(views.APIView):
+class FiltersViewSet(viewsets.GenericViewSet):
     """Не принимает никаких параметров, возвращает все доступные метрики, годы, статьи, части  и категории.
     В Headers запроса должен быть токен `Authorization: Token <token>`"""
     authentication_classes = (TokenAuthentication,)
@@ -31,15 +31,12 @@ class FiltersViewSet(views.APIView):
             return "Воинские преступления"
         return re.search(r'(\d{3}(\.\d{1}|))', part).group()
     
-    def get(self, request, *args, **kwargs): 
+    def list(self, request, *args, **kwargs): 
         df = pd.DataFrame(Part.objects.order_by('part').values())
         
         parts = df.drop_duplicates(subset=['part'])['part'].values.tolist()
-        
         clauses = sorted(list(set([self.get_clause(part) for part in parts])))
-        
         years = df.drop_duplicates(subset=['year'])['year'].values.tolist()
-        
         categories = df.drop_duplicates(subset=['category']).dropna()['category'].values.tolist()
         
         df.loc[:, 'params'] = df.apply(lambda row: list(row.parameters.keys()), axis=1)
@@ -55,3 +52,26 @@ class FiltersViewSet(views.APIView):
         }
         return response.Response(data)
 
+class AggregatedDataViewSet(viewsets.GenericViewSet):
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+    queryset = Part.objects.all()
+    filterset_fields = ['part', 'year']
+
+    def get_queryset(self):
+        filters = {}
+        
+        year = self.request.query_params.get('year')
+        if year:
+            filters['year__in'] = year.split(',')
+
+        part = self.request.query_params.get('part')
+        if part:
+            filters['part__in'] = part.split(',')
+
+        category = self.request.query_params.get('category')
+        if category:
+            filters['category__in'] = category.split(',') if category else None
+
+        qs = self.queryset.filter(**filters)
+        return qs
