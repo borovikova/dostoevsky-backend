31b1d4e6935041b284f7aa8c20a8a5aa84966874
Ekaterina Borovikova <borovikova@zoho.eu>
2021-01-06 11:10:53 +0300

    Aggregated data endpoint

--- app/part/models.py
+++ app/part/models.py
@@ -1,6 +1,36 @@
 import re
 
 from django.db import models
+from django.db.models.functions import Cast
+from django.db.models.fields.json import KeyTextTransform
+
+class AggregatedDataQuerySet(models.QuerySet):
+    def filter_and_aggregate(self, year=None, part=None, category=None, parameters=[], groupby=[]):
+        filters = {}
+        if year:
+            filters['year__in'] = year
+        if part:
+            filters['part__in'] = part
+        if category:
+            filters['category__in'] = category
+        qs = self.filter(**filters)
+
+        annotations = dict(
+            zip(
+                [f'{p}_' for p in parameters], 
+                map(lambda x: Cast(KeyTextTransform(x, "parameters"), models.IntegerField()), parameters)
+                )
+            )
+        qs = qs.annotate(**annotations)
+        annotations = dict(
+            zip(
+                parameters,
+                [models.Sum(f'{p}_') for p in parameters]
+            )
+        )
+        qs = qs.values(*groupby).annotate(**annotations).order_by()
+        return qs
+
 
 
 class Part(models.Model):
@@ -11,6 +41,8 @@ class Part(models.Model):
     year = models.PositiveSmallIntegerField(verbose_name="год")
     category = models.CharField(max_length=255, null=True, verbose_name='категория статьи')
     parameters = models.JSONField(verbose_name='параметры статьи')
+
+    objects = AggregatedDataQuerySet.as_manager()
 
     def __str__(self):
         return f"{self.part} {self.name}"
--- app/part/views.py
+++ app/part/views.py
@@ -1,10 +1,6 @@
+import json
 import re
 import pandas as pd
-import datetime
-
-from django.db.models import IntegerField, Sum
-from django.db.models.functions import Cast
-from django.db.models.fields.json import KeyTransform
 
 from rest_framework import generics, authentication, permissions
 from rest_framework.authentication import TokenAuthentication
@@ -13,7 +9,7 @@ from rest_framework.settings import api_settings
 from rest_framework import viewsets, mixins, response, views
 
 from .models import Part
-from part import serializers
+from .serializers import PartSerializer
 
 class PartViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
     """Не принимает никаких параметров, возвращает все имеющиеся в базе данные одним ответом.
@@ -21,7 +17,7 @@ class PartViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
     queryset = Part.objects.all()
-    serializer_class = serializers.PartSerializer
+    serializer_class = PartSerializer
 
 
 class FiltersViewSet(viewsets.GenericViewSet):
@@ -64,63 +60,41 @@ class AggregatedDataViewSet(viewsets.GenericViewSet):
     queryset = Part.objects.all()
     uncountable = ["addTotalPersons","addTotalOffences","addAcquittalPersons","addAcquittalOffences","addDismissalPersons","addDismissalOffences","addDismissalOtherPersons","addDismissalOtherOffences","addUnfitToPleadPersons","addUnfitToPleadOffences"]
 
-    def filter_queryset(self):
-        filters = {}
+    def list(self, request, *args, **kwargs):
 
         year = self.request.query_params.get('year')
         if year:
-            filters['year__in'] = year.split(',')
+            year = year.split(',')
 
         part = self.request.query_params.get('part')
         if part:
-            filters['part__in'] = part.split(',')
-
+            part = part.split(',')
+        
         category = self.request.query_params.get('category')
         if category:
-            filters['category__in'] = category.split(',')
-
-        qs = self.queryset.filter(**filters)
-        return qs
-    
-    def annotate_queryset(self, qs, parameters, breakdowns):
-        # TODO: перенести в метод qs менеджера
-        annotations = dict(
-            zip(
-                [f'{p}_' for p in parameters], 
-                map(lambda x: Cast(KeyTextTransform(x, "parameters"), IntegerField()), parameters)
-                )
-            )
-        qs = qs.annotate(**annotations)
-
-        annotations = dict(
-            zip(
-                parameters,
-                [Sum(f'{p}_') for p in parameters]
-            )
-        )
-        qs = qs.values(*breakdowns).annotate(**annotations).order_by()
-        return qs
-
-    def list(self, request, *args, **kwargs):
-        # print(set(self.get_queryset().values_list('year', flat=True)))
-
-        qs = self.filter_queryset()
-
+            category = category.split(',')
+        
         params = self.request.query_params.get('param')
-        if params:
-            params = params.split(',')
-            params = [p for p in params if p not in self.uncountable]
+        params = params.split(',') if params else []
+        countable_params = [p for p in params if p not in self.uncountable]
 
         breakdowns = self.request.query_params.get('breakdowns')
-        if breakdowns:
-            breakdowns = breakdowns.split(',')
+        breakdowns = breakdowns.split(',') if breakdowns else []
+        grouby_params = {
+            'year': 'part',
+            'part': 'year'
+        }
+        groupby = [grouby_params[_] for _ in breakdowns if _ in grouby_params.keys()]
+        qs = self.queryset.filter_and_aggregate(year, part, category, countable_params, groupby)
+        
+        if not breakdowns:
+            qs = qs.values('part', 'name', 'year', 'category', 'parameters')
 
-        qs = self.annotate_queryset(qs, params, breakdowns)
+        data = list(qs)
 
-        print(qs)
-        # TODO: вернуть None для неисчисляемых
-        return response.Response([])
+        def add_uncountable_params(row):
+            row.update({param: None for param in params if param in self.uncountable})
+            return row
+        data = list(map(add_uncountable_params, data))
 
-
-
-
+        return response.Response(data)
